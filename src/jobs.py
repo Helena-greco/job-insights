2909b601e2e59b6cf4a816b14a8fd4d4aceb11ac
Helena <nenafgreco@hotmail.com>
2022-06-02 16:37:36 -0300

    reading csv file and create a list

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,15 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
+    with open("jobs.csv", encoding = "utf-8") as file:
+      jobs = csv.DictReader(file)
+      job_list = []
+      for job in jobs:
+        job_list.append(job)
+
+    return job_list
     """Reads a file from a given path and returns its contents.
 
     Parameters
@@ -15,4 +22,3 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
