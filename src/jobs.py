4541b5752351d7af4073a249da495b03fec4f696
Helena <nenafgreco@hotmail.com>
2022-06-02 16:40:11 -0300

    correcting lint

--- src/jobs.py
+++ src/jobs.py
@@ -4,10 +4,10 @@ import csv
 @lru_cache
 def read(path):
     with open("jobs.csv", encoding = "utf-8") as file:
-      jobs = csv.DictReader(file)
-      job_list = []
-      for job in jobs:
-        job_list.append(job)
+        jobs = csv.DictReader(file)
+        job_list = []
+        for job in jobs:
+            job_list.append(job)
 
     return job_list
     """Reads a file from a given path and returns its contents.
