76085a7d3b5598944c598bdedb2e147b90714c39
Helena <nenafgreco@hotmail.com>
2022-06-07 17:27:23 -0300

    corrigindo path do req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("src/counter.py", "Python") == 1639
-    assert count_ocurrences("src/counter.py", "Javascript") == 122
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
